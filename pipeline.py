c443460374b771e91351e940cddd57ee2c0471fa
Aaron <aaronpcoffey@gmail.com>
2025-08-12 01:02:48 -0500

    Pipeline now works

--- pipeline.py
+++ pipeline.py
@@ -2,63 +2,103 @@ import os
 import requests
 import pandas as pd
 import json
-from sqlalchemy import create_engine
+from datetime import datetime
+from sqlalchemy import create_engine, Integer, DateTime, Float, String, Boolean
+from sqlalchemy.dialects.postgresql import JSONB
 from dotenv import load_dotenv
 
 load_dotenv()
 api_url = os.getenv("API_URL")
 api_key = os.getenv("API_KEY")
 database_url = os.getenv("DATABASE_URL")
-series_ids = os.getenv("SERIES_IDS").split(",")
+series_ids = os.getenv("SERIES_IDS").split(",") # type: ignore
 start_year = os.getenv("START_YEAR")
 end_year = os.getenv("END_YEAR")
 
 def fetch_data():
+    """
+    Uses pythons request library to make a post call to our API url.
+    Raises HTTPError if unsuccessful.
+    
+    Returns
+    --------
+    json object of the response data
+    """
     headers = {'Content-type': 'application/json'}
     data = json.dumps({"seriesid": series_ids, "startyear":start_year, "endyear":end_year, "catalog":True, "registrationkey":api_key})
-    response = requests.post(api_url, data=data, headers=headers)
-    print(f"headers: {headers} \ndata:{data} \nresponse:{response} \napi_url:{api_url} \nseries_ids:{series_ids}")
+    response = requests.post(api_url, data=data, headers=headers) # type: ignore
     response.raise_for_status()
     return response.json()
 
-def transform_data(json_data):
+def transform_data(json_data)->pd.DataFrame:
+    """
+    Takes json data from API call and transforms it into a dataframe.
+    BLS has different cassing for catalog vs data k,v pairs (Very annoying) so everything has been converted to lower
+    """
     series = json_data.get("Results", {}).get("series", [])
     if not series:
         raise ValueError("No series data found in API response")
-
     all_data = []
     for s in series:
+        title_info_dict = s.get("catalog", {})
+        title_info_dict = {k.replace("_", ""): v for k,v in title_info_dict.items()}
         for entry in s.get("data", []):
-            row = entry.copy()
-            row["seriesID"] = s.get("seriesID")
-            all_data.append(row)
-
+            row_dict = entry.copy()
+            row_dict["seriesid"] = s.get("seriesID")
+            row_dict.update(title_info_dict)
+            all_data.append(row_dict)
     df = pd.DataFrame(all_data)
     df.columns = df.columns.str.lower()
-
-    for col in df.columns:
-        if df[col].apply(lambda x: isinstance(x, dict)).any():
-            print(f"Converting column '{col}' from dict to JSON string")
-            df[col] = df[col].apply(json.dumps)
-
-    if "year" in df.columns and "period" in df.columns:
-        df["date"] = pd.to_datetime(df["year"] + df["period"].str[1:], format="%Y%m")
-
-    print(f"DATA FRAME:\n{df.head()}")
+    df["latest"] = df["latest"].fillna(False).astype(bool)
+    df["latest"] = df["latest"].replace("true", True).astype(bool)
     return df
 
+def get_json_columns(df)->dict:
+    """
+    Looks through the data frame for columns that 
+    need to be marked as JSONB type
+
+    Argumnets
+    ----------
+    df: pandas DF
+    """
+    jsonb_columns = {}
+    for col in df.columns:
+        if df[col].apply(lambda x: isinstance(x, (dict, list))).any():
+            jsonb_columns[col] = JSONB 
+    return jsonb_columns
+
 def save_to_postgres(df, table_name="api_data"):
-    engine = create_engine(database_url)
+    print("saving data to postgres...")
+    engine = create_engine(database_url) # type: ignore
     with engine.begin() as connection:
-        df.to_sql(table_name, con=connection, if_exists="replace", index=False)
+        df.to_sql(
+            table_name, 
+            con=connection, 
+            if_exists="replace", 
+            index=False,
+            dtype={
+                "year": Integer,
+                "period": String,
+                "periodname": String,
+                "value": Float,
+                "latest": Boolean,
+                "footnotes": JSONB,
+                "seriesid": String,
+                "date": DateTime
+            }
+        )
     engine.dispose()
     print(f"Saved {len(df)} rows to {table_name}")
 
 def main():
+    start = datetime.now()
     print("Running pipeline.....")
     data = fetch_data()
     df = transform_data(data)
     save_to_postgres(df)
+    end = datetime.now()
+    print(f"Pipeline completed in {end-start} seconds")
 
 if __name__ == "__main__":
     main()
